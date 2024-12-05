b8d66bcbb3ffca0bfc791d992f08e6811e5ef121
CryptoMorki3s <cryptomorki3s@gmail.com>
2024-12-05 15:35:31 +0100

    Initial Change

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import react from "@vitejs/plugin-react"
-import { defineConfig, loadEnv } from "vite"
+import { defineConfig} from "vite"
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
