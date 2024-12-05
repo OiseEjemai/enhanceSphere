89f466d4eefa3751eb3ab64fe28ef9dffc4a98d9
CryptoMorki3s <cryptomorki3s@gmail.com>
2024-12-05 15:34:33 +0100

    Initial Change

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import path from 'path'
 import react from "@vitejs/plugin-react"
 import { defineConfig, loadEnv } from "vite"
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills(), viteTsconfigPaths()],
+  plugins: [react(), viteTsconfigPaths()],
 })
